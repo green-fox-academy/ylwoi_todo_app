7fedc8f3d78cfe2e12b410e274817a09d5c5e354
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-06 13:26:47 +0200

    command argument interpreter statements added

--- todo.py
+++ todo.py
@@ -8,6 +8,16 @@ class ArgumentReader():
         self.args = sys.argv
         if len(self.args) == 1:
             self.print_info()
+        else:
+            command = self.args[1:]
+            if command[0] == '-l':
+                print("-l")
+            if command[0] == '-a':
+                print('-a')
+            if command[0] == '-r':
+                print('-r')
+            if command[0] == '-c':
+                print('-c')
 
     def print_info(self):
         print("Python Todo application \n======================= \n \nCommand line arguments: \n-l   Lists all the tasks \n-a   Adds a new task \n-r   Removes an task \n-c   Completes an task ")
