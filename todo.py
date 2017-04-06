a26abdf382b890b8caa4b036cd964992ec030a8a
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-06 13:29:05 +0200

    info printer method deleted and moved

--- todo.py
+++ todo.py
@@ -7,7 +7,7 @@ class ArgumentReader():
     def __init__(self):
         self.args = sys.argv
         if len(self.args) == 1:
-            self.print_info()
+            print("Python Todo application \n======================= \n \nCommand line arguments: \n-l   Lists all the tasks \n-a   Adds a new task \n-r   Removes an task \n-c   Completes an task ")
         else:
             command = self.args[1:]
             if command[0] == '-l':
@@ -18,9 +18,6 @@ class ArgumentReader():
                 print('-r')
             if command[0] == '-c':
                 print('-c')
-
-    def print_info(self):
-        print("Python Todo application \n======================= \n \nCommand line arguments: \n-l   Lists all the tasks \n-a   Adds a new task \n-r   Removes an task \n-c   Completes an task ")
 
 example = ArgumentReader()
 
