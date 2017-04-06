07e6b7f6580ff7151a2a67c3f80ac80b98d04a2e
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-06 21:34:13 +0200

    Task checker class created and works

--- todo.py
+++ todo.py
@@ -7,7 +7,8 @@ class ArgumentReader():
     def __init__(self):
         self.args = sys.argv
         if len(self.args) == 1:
-            print("Python Todo application \n======================= \n \nCommand line arguments: \n-l   Lists all the tasks \n-a   Adds a new task \n-r   Removes an task \n-c   Completes an task ")
+            print("Python Todo application \n======================= \n \nCommand line arguments: \n-l   Lists all the tasks \n-a"
+                  "   Adds a new task \n-r   Removes an task \n-c   Completes an task ")
         else:
             self.command = self.args[1:]
             if self.command[0] == '-l':
@@ -17,7 +18,7 @@ class ArgumentReader():
             if self.command[0] == '-r':
                 del_task = DeleteTask()
             if self.command[0] == '-c':
-                print('-c')
+                check_task = CheckTask()
 
     def reader(self):
         self.db_file = open('DB.txt', 'r+')
@@ -34,9 +35,9 @@ class ListTasks(ArgumentReader):
             print('No todos for today! :)')
         else:
             for i in range(len(self.db_list)):
-                if self.db_list[i][0] == "0 ":
+                if self.db_list[i][0] == "0":
                     self.db_list[i][0] = '[ ]'
-                if self.db_list[i][0] == "1 ":
+                if self.db_list[i][0] == "1":
                     self.db_list[i][0] = '[x]'
                 print(i+1, ' - ', self.db_list[i][0], self.db_list[i][1])
 
@@ -56,6 +57,21 @@ class DeleteTask(ArgumentReader):
         self.db_file.close()
         self.f = open('DB.txt', 'w')
         for i in range(len(lines)):
+            if i != int(self.command[1])-1:
+                self.f.write(lines[i])
+        self.f.close()
+
+
+class CheckTask (ArgumentReader):
+    def __init__(self):
+        self.command = sys.argv[1:]
+        self.db_file = open('DB.txt', 'r')
+        lines = self.db_file.readlines()
+        self.db_file.close()
+        self.f = open('DB.txt', 'w')
+        for i in range(len(lines)):
+            if i == int(self.command[1])-1:
+                self.f.write(lines[i].replace('0', '1', 1))
             if i != int(self.command[1])-1:
                 self.f.write(lines[i])
         self.f.close()
